f3b146735dcf7a96e16a9e251246f8adcfa5132a
JP Lecot <jplecot26@gmail.com>
2026-03-18 20:24:43 -0300

    1er Ejercicio Introductorio

---
+++ Actividades_Introductorias/ejercicio1.py
@@ -0,0 +1,6 @@
+# Años en los que cumplira 18,21 y 100.
+año_nacimiento = int(input ("Ingrese el año de nacimiento: "))
+
+print (f"En el año {año_nacimiento+18} tendrá 18 años")
+print (f"En el año {año_nacimiento+21} tendrá 21 años")
+print (f"En el año {año_nacimiento+100} tendrá 100 años")
