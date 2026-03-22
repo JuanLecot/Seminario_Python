22503e3ed404951fa3388904e9150e6629ce532c
JP Lecot <jplecot26@gmail.com>
2026-03-22 19:13:39 -0300

    Actualizacion de Menu y Estructura de Categorias

--- Entrega_Ahorcado/questions.py
+++ Entrega_Ahorcado/questions.py
@@ -1,26 +1,27 @@
 import random
 
-#Diccionario 
+#Diccionario: Uso de Tuplas (clave = (codigo , nombre))
 categorias_ahorcado = {
-    "Lenguajes de programacion": [
-        "python", "java", "pascal", "javascript", "ruby", "c++"
+    (0,"Finalizar"): [],
+    (1,"Lenguajes de programacion"): [
+        "python", "java", "pascal", "javascript", "ruby", "kotlin"
     ],
-    "Fundamentos de programacion": [
+    (2,"Fundamentos de programacion"): [
         "programa", "variable", "funcion", "bucle", "algoritmo", "sintaxis", "compilador"
     ],
-    "Tipos y estructuras de datos": [
+    (3,"Tipos y estructuras de datos"): [
         "cadena", "entero", "lista", "booleano", "flotante", "diccionario", "tupla"
     ],
-    "Algoritmos avanzados": [
+    (4,"Algoritmos avanzados"): [
         "dijkstra", "recursividad", "grafos", "busqueda", "ordenamiento"
     ],
-    "Desarrollo de software": [
+    (5,"Desarrollo de software"): [
         "framework", "libreria", "repositorio", "interfaz", "depuracion", "api"
     ],
-    "Arquitectura de computadoras": [
+    (6,"Arquitectura de computadoras"): [
         "motherboard", "procesador", "registros", "cache", "transistores", "perifericos"
     ],
-    "Ramas de la matematica": [
+    (7,"Ramas de la matematica"): [
         "algebra", "calculo", "estadistica", "geometria", "aritmetica"
     ]
 }
@@ -29,68 +30,91 @@ categorias_ahorcado = {
 print("¡Bienvenido al Ahorcado!")
 
 print()
-
 print()
-print ("--- MENU ---")
-print("+ Seleccione una de las siguientes categorias: ")
+# Menu de Juego
+while True:
+    print ("--- MENU ---")
+    print("+ Seleccione una de las siguientes categorias: ")
+    for categoria in categorias_ahorcado:
+        clave_numero = categoria[0]
+        clave_nombre = categoria[1]
+        print(f"{clave_numero} : {clave_nombre}")
 
-for categoria in categorias_ahorcado:
-    print (f"- {categoria}")
-categoria_seleccionada = input("Ingrese la categoria: ")
-rondas = int (input (f"Ingrese la cantidad de rondas a jugar (Maximo {len(categorias_ahorcado[categoria_seleccionada])}): "))
+    categoria_seleccionada = int(input("Seleccione el numero de categoria: "))
 
-palabras = random.sample(categorias_ahorcado[categoria_seleccionada],rondas)
+    if categoria_seleccionada == 0:
+        print("Juego Finalizado.")
+        break
+    elif categoria_seleccionada>=1 and categoria_seleccionada<=7:
+        #Busqueda Tupla Seleccionada
+        tupla = ()
+        for categoria in categorias_ahorcado:
+            if categoria[0] == categoria_seleccionada:
+                tupla = categoria
+                break
 
-for i in range(len(palabras)):
-    print()
-    print(f"RONDA {i+1}")
+        #Rondas y Nueva Lista
+        rondas = int (input (f"Ingrese la cantidad de rondas a jugar (Maximo {len(categorias_ahorcado[tupla])}): "))
+        palabras = random.sample(categorias_ahorcado[tupla],rondas)
 
-    word = palabras[i]
 
-    guessed = []
-    attempts = 6
-    puntaje = int(0)
+        for i in range(len(palabras)):
+            print()
+            print(f"RONDA {i+1}")
 
-    while attempts > 0:
-        # Mostrar progreso: letras adivinadas y guiones para las que faltan
-        progress = ""
-        for letter in word:
-            if letter in guessed:
-                progress += letter + " "
+            word = palabras[i]
+
+            guessed = []
+            attempts = 6
+            puntaje = 0
+
+            while attempts > 0:
+                # Mostrar progreso: letras adivinadas y guiones para las que faltan
+                progress = ""
+                for letter in word:
+                    if letter in guessed:
+                        progress += letter + " "
+                    else:
+                        progress += "_ "
+                print(progress)
+
+                # Verificar si el jugador ya adivinó la palabra completa
+                if "_" not in progress:
+                    print ("-"*10)
+                    print("¡Ganaste!")
+                    puntaje += 6 #Puntaje x Ganar
+                    break
+
+                print(f"Intentos restantes: {attempts}")
+                print(f"Letras usadas: {', '.join(guessed)}")
+
+                letter = input("Ingresá una letra: ")
+                
+                if letter in guessed:
+                    print("Ya usaste esa letra.")
+                elif letter in word:
+                    guessed.append(letter)
+                    print("¡Bien! Esa letra está en la palabra.")
+                
+                # Caso Invalido: Caracter Especial, Numero o Palabra
+                elif (letter < "a" or letter > "z") or (len(letter) > 1):
+                    print ("Entrada No Valida.")
+
+                else: 
+                    guessed.append(letter)
+                    attempts -= 1
+                    print("Esa letra no está en la palabra.")
+                    puntaje -= 1 
+
+                print()
             else:
-                progress += "_ "
-        print(progress)
+                print ("-"*10)
+                print(f"¡Perdiste! La palabra era: {word}")
+                puntaje = 0 # Puntaje x Perder
 
-        # Verificar si el jugador ya adivinó la palabra completa
-        if "_" not in progress:
-            print("¡Ganaste!")
-            puntaje += 6 #Puntaje x Ganar
-            break
-
-        print(f"Intentos restantes: {attempts}")
-        print(f"Letras usadas: {', '.join(guessed)}")
-
-        letter = input("Ingresá una letra: ")
-        
-        if letter in guessed:
-            print("Ya usaste esa letra.")
-        elif letter in word:
-            guessed.append(letter)
-            print("¡Bien! Esa letra está en la palabra.")
-        
-        # Caso Invalido: Caracter Especial, Numero o Palabra
-        elif (letter < "a" or letter > "z") or (len(letter) > 1):
-            print ("Entrada No Valida.")
-
-        else: 
-            guessed.append(letter)
-            attempts -= 1
-            print("Esa letra no está en la palabra.")
-            puntaje -= 1 
-
-        print()
+            print(f"Tu Puntaje es {puntaje}")
+            print("-"*10)
     else:
-        print(f"¡Perdiste! La palabra era: {word}")
-        puntaje = 0 # Puntaje x Perder
-
-    print(f"Tu Puntaje es {puntaje}")
+        print()
+        print("Opcion Invalida.")
+        print()
